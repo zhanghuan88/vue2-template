ef48de54fdfe2470c9381f4b79526ebd04239177
zhanghuan <363416071@qq.com>
2022-04-21 17:35:00 +0800

    feat(project): 项目改动
    优化次菜单栏

--- src/framework/layout/components/TopNavigation.vue
+++ src/framework/layout/components/TopNavigation.vue
@@ -1,26 +1,24 @@
 <template>
   <div class="top-navigation">
-    <div class="top-navigation-tabs">
-      <el-tabs v-model="currentTagPath" type="card" @tab-remove="removeTab" @tab-click="tabClick">
-        <el-tab-pane v-for="(item,index) in showTags" :key="item.path" :label="item.name" :name="item.path"
-                     :closable="index!==0">
-        </el-tab-pane>
-      </el-tabs>
-    </div>
-    <div class="top-navigation-tool">
-      <el-dropdown>
-        <span class="el-dropdown-link">
-          下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
-        </span>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item icon="el-icon-plus">黄金糕</el-dropdown-item>
-          <el-dropdown-item icon="el-icon-circle-plus">狮子头</el-dropdown-item>
-          <el-dropdown-item icon="el-icon-circle-plus-outline">螺蛳粉</el-dropdown-item>
-          <el-dropdown-item icon="el-icon-check">双皮奶</el-dropdown-item>
-          <el-dropdown-item icon="el-icon-circle-check">蚵仔煎</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-    </div>
+    <el-tabs v-model="currentTagPath" class="top-navigation-tabs" type="card" @tab-remove="removeTab"
+             @tab-click="tabClick">
+      <el-tab-pane v-for="(item,index) in showTags" :key="item.path" :label="item.name" :name="item.path"
+                   :closable="index!==0">
+      </el-tab-pane>
+    </el-tabs>
+    <el-dropdown class="top-navigation-tool">
+      <span class="top-navigation-tool--icon">
+        <i class="box box-t"></i>
+        <i class="box box-b"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item icon="el-icon-plus">黄金糕</el-dropdown-item>
+        <el-dropdown-item icon="el-icon-circle-plus">狮子头</el-dropdown-item>
+        <el-dropdown-item icon="el-icon-circle-plus-outline">螺蛳粉</el-dropdown-item>
+        <el-dropdown-item icon="el-icon-check">双皮奶</el-dropdown-item>
+        <el-dropdown-item icon="el-icon-circle-check">蚵仔煎</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
   </div>
 </template>
 
@@ -107,17 +105,67 @@ export default {
 
 <style scoped lang="scss">
 .top-navigation {
+  position: relative;
+  box-sizing: border-box;
   display: flex;
   height: $g-top-tabs-height;
   padding: 0 20px;
+  align-content: center;
+  align-items: center;
+  justify-content: space-between;
+  user-select: none;
 
   .top-navigation-tabs {
-    flex-grow: 1;
+
+    ::v-deep .el-tabs__header {
+      margin-bottom: 0;
+    }
   }
 
   .top-navigation-tool {
     margin-left: 20px;
-    flex-shrink: 0;
+
+    .top-navigation-tool--icon {
+      display: inline-block;
+      color: #9a9a9a;
+      cursor: pointer;
+      transition: transform .3s ease-out;
+
+      &:hover {
+        transform: rotate(90deg);
+
+        .box:before, .box:after {
+          background-color: #1890ff;
+        }
+      }
+
+      .box {
+        position: relative;
+        display: block;
+        width: 14px;
+        height: 8px;
+
+        &:before {
+          position: absolute;
+          top: 2px;
+          left: 0;
+          width: 6px;
+          height: 6px;
+          content: "";
+          background: #9a9a9a;
+        }
+
+        &:after {
+          position: absolute;
+          top: 2px;
+          left: 8px;
+          width: 6px;
+          height: 6px;
+          content: "";
+          background: #9a9a9a;
+        }
+      }
+    }
   }
 }
 </style>
