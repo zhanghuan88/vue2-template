20d44ec473910a9f1372dbb2b7353df245916110
zhanghuan <363416071@qq.com>
2022-04-26 17:18:05 +0800

    feat(project): 项目改动
    公用组件自动注册

--- src/main.js
+++ src/main.js
@@ -4,16 +4,15 @@ import router from './router'
 import store from "./store/index";
 import './styles/index.scss';
 import VueMeta from 'vue-meta'
-import SvgIcon from '@/components/SvgIcon'
 import ELEMENT from 'element-ui';
+import "@/components";
 
-// 根据CDN配置条件编译
 Vue.use(ELEMENT);
+
 // 自动加载 svg 图标
 const req = require.context('./assets/icons', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
-Vue.component(SvgIcon.name, SvgIcon)
 Vue.use(VueMeta)
 
 const app = new Vue({
