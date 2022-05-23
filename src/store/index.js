c24f1a76ae2e3991f89bae7efb5f8547df5271f5
zhanghuan <363416071@qq.com>
2022-05-23 09:37:01 +0800

    feat(prod): eslint
    eslint

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import project from '@/store/modules/project'
-import user from '@/store/modules/user'
-import getters from '@/store/getters'
-import menu from '@/store/modules/menu'
+import project from "@/store/modules/project";
+import user from "@/store/modules/user";
+import getters from "@/store/getters";
+import menu from "@/store/modules/menu";
 
 Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     project,
     user,
-    menu
+    menu,
   },
-  getters
+  getters,
 });
